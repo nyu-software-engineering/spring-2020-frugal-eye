d42b8580569c12b7761137d7b45c8807f7b0e8b0
evillafr <villafranca.eliana@gmail.com>
2020-03-23 14:46:21 -0700

    add routing so Login page loads as the default

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Login from './Login'
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <Login />
+                </Route>
+
+            </Switch>
+        </Router>
     </div>
   );
 }
 
+// make this available to other modules as an import
 export default App;
