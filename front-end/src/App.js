d637c36ea8f9ffa3ba4fd6dd558bdd8412b4a17c
MGao535 <mmg535@nyu.edu>
2020-03-22 19:18:11 -0500

    first upload

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const http = require('http');
+const express = require('express');
+const app = express();
+const path = require("path");
+require('./db');
 
-export default App;
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'hbs');
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+const mongoose = require('mongoose');
+const Showdb = mongoose.model('Showdb');
+
+app.get('/', (req, res) => {
+  print(
+    <h1>Hello World</h1>
+    );
+});
