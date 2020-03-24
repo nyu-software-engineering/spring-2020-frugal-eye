c692059d96c76ab054feeac6d0ed50342e1b43fa
Charmaine Chan <charmaine.chan08@gmail.com>
2020-03-24 16:23:42 -0400

    implemented add ingredients button and input fields

--- front-end/src/AddIngredients.js
+++ front-end/src/AddIngredients.js
@@ -1,10 +1,25 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './AddIngredients.css';
 
 const AddIngredients = (props) =>{
+    const [ingredient, setIngredient] = useState("");
+
+    function handleSubmit(event) {
+        alert("You've added " + ingredient);
+        event.preventDefault();
+    }
+
     return (
         <div>
             <h1>What's in your fridge?</h1>
+            <div id="display-ingredients">
+            </div>
+            <form onSubmit={handleSubmit}>
+                <p>Input your ingredients:</p>
+                <input type="text" value={ingredient} onChange={e => setIngredient(e.target.value)}/>
+                <br></br><input type="submit" value="Add"/>
+                
+            </form>
         </div>
     );
 }
