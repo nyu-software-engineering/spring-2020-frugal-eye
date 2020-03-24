9d5f643b04f20b41d3ef88db9e780cc8b57e8884
Charmaine Chan <charmaine.chan08@gmail.com>
2020-03-24 13:57:22 -0400

    created the add ingredients page

---
+++ front-end/src/AddIngredients.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './AddIngredients.css';
+
+const AddIngredients = (props) =>{
+    return (
+        <div>
+            <h1>What's in your fridge?</h1>
+        </div>
+    );
+}
+export default AddIngredients;
