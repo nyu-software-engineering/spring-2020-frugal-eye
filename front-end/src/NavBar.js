505d0ab370d3147e282abf05c18fe65f56068841
Charmaine Chan <charmaine.chan08@gmail.com>
2020-04-12 19:54:06 -0400

    changed nav bar with title on left, nav items on right

--- front-end/src/NavBar.js
+++ front-end/src/NavBar.js
@@ -4,24 +4,14 @@ import './NavBar.css'
 const NavBar = (props) => {
 
     return (
-    // <div>
-    //    <div className="navbar">
-    //         <ul className="nav">
-    //             <li><a href="/home">Home</a></li>
-    //             <li><a href="/add-ingredients">Input Ingredients</a></li>
-    //             <li><a href="/favoritelist">Favorites</a></li>
-    //             <li className="title">Sprouts</li>
-    //         </ul>
-    //     </div>
         <div id="centeredmenu">
-            <div className="title2">Sprouts</div>
+           <span className="title">Sprouts</span>
             <ul>
                 <li><a href="/home">Home</a></li>
                 <li><a href="/add-ingredients">Input Ingredients</a></li>
                 <li><a href="/favoritelist">Favorites</a></li>
             </ul>
         </div>
-    // </div>
     );
   }
   export default NavBar;
