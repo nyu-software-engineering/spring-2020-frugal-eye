b2da4645bd2f3178c35741e2055f64251e8a1569
Charmaine Chan <charmaine.chan08@gmail.com>
2020-04-12 19:33:03 -0400

    centered and changed layout of nav bar

--- front-end/src/NavBar.js
+++ front-end/src/NavBar.js
@@ -4,15 +4,24 @@ import './NavBar.css'
 const NavBar = (props) => {
 
     return (
-       <div className="navbar">
-            <ul className="nav">
+    // <div>
+    //    <div className="navbar">
+    //         <ul className="nav">
+    //             <li><a href="/home">Home</a></li>
+    //             <li><a href="/add-ingredients">Input Ingredients</a></li>
+    //             <li><a href="/favoritelist">Favorites</a></li>
+    //             <li className="title">Sprouts</li>
+    //         </ul>
+    //     </div>
+        <div id="centeredmenu">
+            <div className="title2">Sprouts</div>
+            <ul>
                 <li><a href="/home">Home</a></li>
                 <li><a href="/add-ingredients">Input Ingredients</a></li>
                 <li><a href="/favoritelist">Favorites</a></li>
-                <li className="title">Sprouts</li>
             </ul>
         </div>
-      
+    // </div>
     );
   }
   export default NavBar;
