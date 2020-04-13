6849167df595885dde759d0a6b00ee5d7896907e
Charmaine Chan <charmaine.chan08@gmail.com>
2020-04-13 01:52:46 -0400

    added nav bar to settings page

--- front-end/src/Settings.js
+++ front-end/src/Settings.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Popup from './components/Popup';
 import './standard.css';
 import axios from 'axios';
+import NavBar from './NavBar.js'
 
 const Settings = (props) => {
     const [username, setUsername] = useState("");
@@ -28,43 +29,43 @@ const Settings = (props) => {
 
     return (
       <div>
-      <button className="back-button" onClick={event => window.location.href='/home'}>Back to home</button>
-      <br></br><br></br>
-      <h1>Settings</h1>
-      <form onSubmit={handleSubmit}>
-        <h4>Change Login Information</h4>
-            <label>
-                <input className="standard_input" type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username"/>
-            </label>
-            <br></br>
-            <br></br>
-            <label>
-                <input className="standard_input" type="password" value={password} onChange={f => setPassword(f.target.value)} placeholder="Password"/>
-            </label>
+          <NavBar/>
+        <br></br><br></br>
+        <h1>Settings</h1>
+        <form onSubmit={handleSubmit}>
+            <h4>Change Login Information</h4>
+                <label>
+                    <input className="standard_input" type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username"/>
+                </label>
+                <br></br>
+                <br></br>
+                <label>
+                    <input className="standard_input" type="password" value={password} onChange={f => setPassword(f.target.value)} placeholder="Password"/>
+                </label>
+                <p></p>
+                <input className="standard_button" type="submit" value="Change" />
+            </form>
             <p></p>
-            <input className="standard_button" type="submit" value="Change" />
-        </form>
-        <p></p>
-        {showPopupAct ?
-        <Popup  
-            text='Account information has been changed'
+            {showPopupAct ?
+            <Popup  
+                text='Account information has been changed'
 
-            closePopup={t => setShowPopupAct(!showPopupAct)}  
-        />
-        :
-        null
-        }  
-        <button className="standard_button" onClick={h => setShowPopupDel(!showPopupDel)}>Clear Favorites</button>
-        {showPopupDel ?
-        <Popup  
-            text='Favorite recipes have been cleared' 
-            closePopup={g => setShowPopupDel(!showPopupDel)}  
-        />
-        :
-        null
-        }
-        <p></p>
-        <button className="standard_button" onClick={event => window.location.href='/'}>Log Out</button>
+                closePopup={t => setShowPopupAct(!showPopupAct)}  
+            />
+            :
+            null
+            }  
+            <button className="standard_button" onClick={h => setShowPopupDel(!showPopupDel)}>Clear Favorites</button>
+            {showPopupDel ?
+            <Popup  
+                text='Favorite recipes have been cleared' 
+                closePopup={g => setShowPopupDel(!showPopupDel)}  
+            />
+            :
+            null
+            }
+            <p></p>
+            <button className="standard_button" onClick={event => window.location.href='/'}>Log Out</button>
 
       </div>
     );
