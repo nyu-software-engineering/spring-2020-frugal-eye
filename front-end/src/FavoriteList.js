db4bfb7e358119f3ef55b95dbb9f196d99bb7414
alextli <alextianhongli@gmail.com>
2020-04-13 23:08:52 -0400

    Adding backend functionality per #55

--- front-end/src/FavoriteList.js
+++ front-end/src/FavoriteList.js
@@ -1,20 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './RecipeList.css'
 import Popup from './components/Popup';
 import './register.css';
+import NavBar from "./NavBar.js"
 import axios from 'axios'
 
 const FavoriteList = (props) => {
-
-  const [matchPopup, setMatchPopup] = useState(true);
-  const data = {};
-  axios.get('http://localhost:3000/favoritelist').then(function(response) {
-            //data = res;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3000/favoritelist').then(function(response) {
             console.log(response.data);
-  }).catch(function(error) {
-            console.log(error);
-  });
-
+            setData(response.data);
+    });
+  }, []);
   return (
 
     //retrieve search params from props
@@ -23,16 +21,13 @@ const FavoriteList = (props) => {
 
     //where props.action is sending to specific recipe page with param = recipe selected
 
-  	<div class = "flex-container">
-            <button className="back-button" onClick={event => window.location.href='/home'}>Back to home</button>
+  	<div className = "flex-container">
+    <NavBar/>
         <br></br>
-      {matchPopup ? <Popup id = 'instructions' 
-        text='Hi! Looks like this is your first time viewing recipes. The ones in green are those that you have all ingredients for, yellow are those you have some for.' 
-        closePopup={(h) => setMatchPopup(!matchPopup)} /> : null}
       {Object.keys(data).map((key, index) => {
         if(data[key].favorite){
           return(
-            <p className = 'recipe' onClick={null}>
+            <p className = 'recipe' onClick={event => window.location.href="/recipe/"+key}>
             {data[key].name}
             <img src = {require("" + data[key].image)} alt = 'image'/>
             </p>
@@ -45,3 +40,47 @@ const FavoriteList = (props) => {
 }
 
 export default FavoriteList;
+
+// import React, {useState} from 'react';
+// import './RecipeList.css'
+// import Popup from './components/Popup';
+// import './register.css';
+// import axios from 'axios'
+
+// const FavoriteList = (props) => {
+
+//   const [matchPopup, setMatchPopup] = useState(true);
+
+//   const getData = async () => {
+//     let res = await axios.get('http://localhost:3000/favoritelist');
+//     const data = res.data;
+//     console.log(data)
+
+//     return (
+
+//       //retrieve search params from props
+//       //.find({ingredients = props.ingredients}, ingredients)
+//       //for {ingredient} in {ingredients}
+
+//       //where props.action is sending to specific recipe page with param = recipe selected
+
+//       <div className = "flex-container">
+//       <button className="back-button" onClick={event => window.location.href='/home'}>Back to home</button>
+//           <br></br>
+//         {Object.keys(data).map((key, index) => {
+//           if(data[key].favorite){
+//             return(
+//               <p className = 'recipe' onClick={null}>
+//               {data[key].name}
+//               <img src = {require("" + data[key].image)} alt = 'image'/>
+//               </p>
+//             )
+//           }else return "";
+//         })}
+
+//       </div>
+//     )
+//   }
+// }
+
+// export default FavoriteList;
--- front-end/src/RecipeList.js
+++ front-end/src/RecipeList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './RecipeList.css'
 import Popup from './components/Popup';
 import NavBar from './NavBar';
@@ -6,13 +6,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './register.css'
 import axios from 'axios'
 const RecipeList = (props) => {
-  const [matchPopup, setMatchPopup] = useState(true);
-  const data = {};
-  axios.get('http://localhost:3000/recipelist').then(function(res) {
-            data = res;
-  }).catch(function(error) {
-            console.log(error);
-  });
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3000/favoritelist').then(function(response) {
+            console.log(response.data);
+            setData(response.data);
+    });
+  }, []);
   return (
     //retrieve search params from props
     //.find({ingredients = props.ingredients}, ingredients)
@@ -21,9 +21,6 @@ const RecipeList = (props) => {
     //onclick the name of the recipe from the recipelist will store the name in props and load recipe page
     <div class = "flex-container">
       <NavBar/>
-      {matchPopup ? <Popup id = 'instructions'
-        text='Hi! Looks like this is your first time viewing recipes. The ones in green are those that you have all ingredients for, yellow are those you have some for.'
-        closePopup={(h) => setMatchPopup(!matchPopup)} /> : null}
       {Object.keys(data).map((key, index) => {
         return(
           <p className = 'recipe' onClick={event => window.location.href="/recipe/"+key}>
