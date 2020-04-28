597cf031a01fa4ee31025e34498bfef23806a007
MGao535 <mmg535@nyu.edu>
2020-04-28 01:27:44 -0500

    implementing api call

--- front-end/src/recipepage.js
+++ front-end/src/recipepage.js
@@ -7,11 +7,64 @@ import './recipepage.css'
 
 const RecipePage = (props) => {
     const [matchPopup, setMatchPopup] = useState(true);
+    const [dat, setData] = useState([]);
+
     const data = require("./Sampledata2");
     let {key} = useParams();
 
-    let id = parseInt(useParams());
+    let id = 716429;
+    let apikey = "097d255cf08b43c38036c60fb487d129";
+    //let id = parseInt(useParams());
+    let url = "https://api.spoonacular.com/recipes/" + id + "/information?apiKey=" + apikey;
     //let {recipeName} = data[id].name;
+
+    let Recipe = {
+        id: Number,
+        recipe_name: String,
+    };
+
+    let useEffect = function(){
+        async function getID() {
+            const result = await axios(url);
+            setData(result.dat);
+            alert(dat.id);
+        }
+
+        getID();
+    }
+
+    //alert(dat.id);
+
+    useEffect();
+    
+
+    // let getJSON = function(url, callback) {
+
+    //     let xhr = new XMLHttpRequest();
+    //     xhr.open('GET', url, true);
+    //     xhr.responseType = 'json';
+        
+    //     xhr.onload = function() {
+        
+    //         var status = xhr.status;
+            
+    //         if (status == 200) {
+    //             callback(null, xhr.response);
+    //         } else {
+    //             callback(status);
+    //         }
+    //     };
+        
+    //     xhr.send();
+    // };
+
+    // getJSON(url,  function(err, data) {
+    //     Recipe.id = data.id;
+    //     Recipe.recipe_name = data.title;
+    //     alert(Recipe.recipe_name);
+    // });
+
+
 
    function favorited(){
 
@@ -24,14 +77,14 @@ const RecipePage = (props) => {
             console.log(error);
         });
 
-		if(data[key].favorite == false){
-			data[key].favorite = true;
-		}
-		else{
-			data[key].favorite = false;
-		}
-		//alert("favorited");
-		return;
+        if(data[key].favorite == false){
+            data[key].favorite = true;
+        }
+        else{
+            data[key].favorite = false;
+        }
+        //alert("favorited");
+        return;
     }
     //onclick the name of the recipe from the recipelist will store the name in props and load this page
     //name = props.recipeName
