2e8b8f799dfbc8c5231eb910f954f3a81eb9e527
MGao535 <mmg535@nyu.edu>
2020-05-04 14:48:59 -0500

    testing different render methods

--- front-end/src/recipepage.js
+++ front-end/src/recipepage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router";
 import Popup from './components/Popup';
 import NavBar from './NavBar';
@@ -6,65 +6,81 @@ import axios from 'axios';
 import './recipepage.css'
 
 const RecipePage = (props) => {
+    
+    const [data, setData] = useState([]);
+    const [instr, setInstr] = useState([]);
     const [matchPopup, setMatchPopup] = useState(true);
-    const [dat, setData] = useState([]);
 
-    const data = require("./Sampledata2");
+    const dat = require("./Sampledata2");
     let {key} = useParams();
 
     let id = 716429;
-    let apikey = "097d255cf08b43c38036c60fb487d129";
+    let apikey = "7c8a8fa894364cee819710b8b1887a74";
     //let id = parseInt(useParams());
-    let url = "https://api.spoonacular.com/recipes/" + id + "/information?apiKey=" + apikey;
+    let ingredientsurl = "https://api.spoonacular.com/recipes/" + id + "/information?apiKey=" + apikey;
+    let instructionsurl = "https://api.spoonacular.com/recipes/" + id + "/analyzedInstructions?apiKey=" + apikey;
     //let {recipeName} = data[id].name;
+
+    useEffect(() => {
+        axios.get(ingredientsurl).then(function(response) {
+            console.log(response);
+            setData(response.data);
+        }).catch(error => {
+            console.log(error);
+        });
+            axios.get(instructionsurl).then(function(response) {
+                setInstr(response.data);
+            }).catch(error => {
+            console.log(error);
+        });
+//        }, []);
+    }, []);
+ 
 
     let Recipe = {
         id: Number,
         recipe_name: String,
+        ingredients: [Object],
+//        instructions: [String]
     };
 
-    let useEffect = function(){
-        async function getID() {
-            const result = await axios(url);
-            setData(result.dat);
-            alert(dat.id);
-        }
+        Recipe.id = data.id;
+        Recipe.recipe_name = data.title;
+        Recipe.ingredients = data.extendedIngredients;
+    try{
+        alert(data.extendedIngredients)
+    }
+    catch{
 
-        getID();
     }
 
-    //alert(dat.id);
+    function ingredients(){
+        if(data != undefined){}
+            return(
+                <ul>
+                    {data.ingredients.map(ingredient => (
+                        <li>{ingredient}</li>
+                    ))}   
+                </ul>
+            )
+        else{
+            return ();
+        }
+    }
 
-    useEffect();
-    
-
-    // let getJSON = function(url, callback) {
-
-    //     let xhr = new XMLHttpRequest();
-    //     xhr.open('GET', url, true);
-    //     xhr.responseType = 'json';
-        
-    //     xhr.onload = function() {
-        
-    //         var status = xhr.status;
-            
-    //         if (status == 200) {
-    //             callback(null, xhr.response);
-    //         } else {
-    //             callback(status);
-    //         }
-    //     };
-        
-    //     xhr.send();
-    // };
-
-    // getJSON(url,  function(err, data) {
-    //     Recipe.id = data.id;
-    //     Recipe.recipe_name = data.title;
-    //     alert(Recipe.recipe_name);
-    // });
-
-
+    function ingredients(){
+        if(data != undefined){}
+            return(
+                <ul>
+                    {instr.map(instruction => (
+                    <li>{instruction}</li>
+                    ))}   
+                </ul>
+            )
+        else{
+            return ();
+        }
+    }
 
    function favorited(){
 
@@ -97,21 +113,13 @@ const RecipePage = (props) => {
             <button className="recipes-button" onClick={event => window.location.href='/recipelist'}>Back to recipes</button>
             <br></br><br></br>
             <div className = "recipe2">
-                <img src = {require("" + data[key].image)} width="200" alt = 'image'/>
-                <h3>{data[key].name}</h3>
+                <img src = {require("" + dat[key].image)} width="200" alt = 'image'/>
+                <h3>{Recipe.recipe_name}</h3>
                 <button onClick={favorited}>Add to Favorites</button>
                 <p>Ingredients:</p>
-                <ul>
-                    {data[key].ingredients.map(ingredient => (
-                        <li>{ingredient}</li>
-                    ))}     
-                </ul>
+                {this.ingredients()}
                 <p>Instructions:</p>
-                <ul>
-                    {data[key].instructions.map(instruction => (
-                        <li>{instruction}</li>
-                    ))}   
-                </ul>
+                {this.instructions()}
             </div>
         </div>
     );
